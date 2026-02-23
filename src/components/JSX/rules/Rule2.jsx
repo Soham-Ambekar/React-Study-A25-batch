92ea0d1ef3d7a8ec02aa00ad6cfdfee5d66959b7
Soham <sohamambekar2002@gmail.com>
2026-02-23 20:29:03 +0530

    Rule 2 jsx

---
+++ src/components/JSX/rules/Rule2.jsx
@@ -0,0 +1,16 @@
+let Rule2 = ()=>{
+
+    return(
+        <>
+        <div style={{backgroundColor : "yellow", fontSize: "30px"}}>Rule2</div>
+        <p>Each and evey tag should be closed like br hr and mode.</p>
+        <p>Para1</p>
+        <br />
+        <hr />
+        <p>para2</p>
+        </>
+    )
+
+}
+
+export default Rule2
