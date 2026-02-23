0780e7a66bd1619cd2997073654ed07d1f0b1dd5
Soham <sohamambekar2002@gmail.com>
2026-02-23 20:28:45 +0530

    jsxIntro.jsx file

---
+++ src/components/JSX/JsxIntro.jsx
@@ -0,0 +1,18 @@
+import Rule1 from "./rules/Rule1";
+import Rule2 from "./rules/Rule2";
+import Rule3 from "./rules/Rule3";
+import Rule4 from "./rules/Rule4";
+
+let JsxIntro = ()=>{
+    return (
+        <div>
+            <h1>JSX intro comp.</h1>
+            <Rule1></Rule1>
+            <Rule2></Rule2>
+            <Rule3></Rule3>
+            <Rule4></Rule4>
+        </div>
+    )
+}
+
+export default JsxIntro;
