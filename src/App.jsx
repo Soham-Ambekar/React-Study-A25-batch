8c9b2ce0bfb38bc06fdda5f34a0da197901edfd4
Soham <sohamambekar2002@gmail.com>
2026-02-23 20:27:52 +0530

    App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import JsxIntro from "./components/JSX/JsxIntro";
+
+
+let App = ()=>{
+  // write js here 
+
+  return (
+    <>
+
+    {/* <div><p>App</p></div> */}
+    <JsxIntro></JsxIntro>
+    
+    </>
+  )
+}
+
+export default App;
