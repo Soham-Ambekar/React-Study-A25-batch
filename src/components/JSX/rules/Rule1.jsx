6e2f126f6f68f8e93ae5acb674ec235d2b51af46
Soham <sohamambekar2002@gmail.com>
2026-02-23 20:28:55 +0530

    Rule1 jsx

---
+++ src/components/JSX/rules/Rule1.jsx
@@ -0,0 +1,14 @@
+let Rule1 = ()=>{
+
+    return (
+        <>
+        <div style={{backgroundColor : "yellow", fontSize: "30px"}}>Rule 1</div>
+        <p>Can't return more than one elements at a time</p>
+        {/* we cant pass multiple elements from jsx so we can take one elment wrapping others inn it... */}
+        
+        </>
+    )
+
+}
+
+export default Rule1
